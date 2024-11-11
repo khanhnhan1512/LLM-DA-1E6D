246f2e5e035c03597b391e3d9cc9adc3bb305f39
APAC\UHY6HC <fixed-term.nhan.nguyenkhanh@bcn.bosch.com>
2024-11-11 15:45:37 +0700

    Test openai

--- test_gpt.py
+++ test_gpt.py
@@ -1,38 +1,5 @@
-from openai import OpenAI
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 import os
 
-YOUR_API_KEY = os.environ.get("OPENAI_API_KEY")
-
-messages = [
-    {
-        "role": "system",
-        "content": (
-            "You are an artificial intelligence assistant and you need to "
-            "engage in a helpful, detailed, polite conversation with a user."
-        ),
-    },
-    {
-        "role": "user",
-        "content": (
-            "How many stars are in the universe?"
-        ),
-    },
-]
-
-client = OpenAI(api_key=YOUR_API_KEY, base_url="https://api.perplexity.ai")
-
-# chat completion without streaming
-response = client.chat.completions.create(
-    model="llama-3.1-sonar-large-128k-online",
-    messages=messages,
-)
-print(response)
-
-# chat completion with streaming
-response_stream = client.chat.completions.create(
-    model="llama-3.1-sonar-large-128k-online",
-    messages=messages,
-    stream=True,
-)
-for response in response_stream:
-    print(response)
+API_KEY = ""
+print(os.environ['OPENAI_API_KEY'])
